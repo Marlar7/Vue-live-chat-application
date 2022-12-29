eb5ddf32ac3f6ddb546dd91fc6cc75238a40ff58
Marlar7 <novenhnin@gmail.com>
2022-12-29 12:53:12 +0630

    project set up and add global css

---
+++ src/views/Welcome.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="welcome container">
+        <P>Welcome</P>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    .welcome{
+        text-align: center;
+        padding:20px 0;
+    }
+</style>
+
+
