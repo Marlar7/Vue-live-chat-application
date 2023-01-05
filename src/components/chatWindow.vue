6b1a6e998cff69a37167a301e3f4227a95439931
Marlar7 <novenhnin@gmail.com>
2023-01-05 10:58:48 +0630

    datefns

--- src/components/chatWindow.vue
+++ src/components/chatWindow.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="chat-window">
-        <div class="messages" v-for="message in messages" :key="message.id">
-            <div class="single">
-                <span class="created-at">{{message.created_at.toDate()}}</span>
+        <div class="messages" >
+            <div class="single" v-for="message in formattedMessages" :key="message.id">
+                <span class="created-at">{{message.created_at}}</span>
                 <span class="name">{{message.name}}</span>
                 <span class="message">{{message.message}}</span>
             </div>
@@ -11,13 +11,14 @@
 </template>
 <script>
 import {db} from "../firebase/config";
+import {formatDistanceToNow} from "date-fns"
 import {ref, computed} from "vue"
 export default {
     setup(){
         let messages= ref([]);
         let formattedMessages= computed(()=>{
             return messages.value.map((msg)=>{
-                let formatTime=
+                let formatTime=formatDistanceToNow(msg.created_at.toDate())
                 return {...msg,created_at:formatTime}
             })
         })
@@ -32,7 +33,7 @@ export default {
             })
             messages.value=results;
         })
-        return{messages};
+        return{messages, formattedMessages};
     }
 }
 </script>
